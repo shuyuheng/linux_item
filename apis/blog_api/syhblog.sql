e0ebc15754781a267a5d12a49b442ba411874f56
shuyuheng <2285128399@qq.com>
2020-02-29 00:12:59 +0800

    55

--- apis/blog_api/syhblog.sql
+++ apis/blog_api/syhblog.sql
@@ -11,7 +11,7 @@
  Target Server Version : 50725
  File Encoding         : 65001
 
- Date: 26/02/2020 01:00:29
+ Date: 29/02/2020 00:05:52
 */
 
 SET NAMES utf8mb4;
@@ -68,12 +68,12 @@ CREATE TABLE `navs`  (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `user_id` int(11) NOT NULL,
   `navs` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '1,1,1,1,1,1',
-  `n1` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '0,0,0',
-  `n2` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '0,0,0',
-  `n3` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '0,0,0',
-  `n4` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '0,0,0',
-  `n5` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '0,0,0',
-  `n6` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '0,0,0',
+  `n1` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '000,000,000',
+  `n2` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '000,000,000',
+  `n3` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '000,000,000',
+  `n4` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '000,000,000',
+  `n5` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '000,000,000',
+  `n6` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '000,000,000',
   `n1_img` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
   `n2_img` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
   `n3_img` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
@@ -81,13 +81,30 @@ CREATE TABLE `navs`  (
   `n5_img` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
   `n6_img` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 10 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of navs
 -- ----------------------------
-INSERT INTO `navs` VALUES (2, 13, '1,1,1,1,1,1', '0,0,0', '0,0,0', '0,0,0', '0,0,0', '0,0,0', '0,0,0', 'http://192.168.0.104:7788/upload_files/2020-2-17/syh.jpg', NULL, NULL, NULL, NULL, NULL);
-INSERT INTO `navs` VALUES (3, 5, '1,0,1,1,0,0', '可爱至极,傻六六在此,标配顶配天仙配', '12345,54321,杀杀杀', '热爱留恋,生活处处有乐趣,拍下人生美好时刻', '爱情～,一日三餐四季,你我足以', '旅行的意义,生命~,川流不息', '000,000,000', '/upload_files/2020-2-26/1582649871148.jpeg', NULL, '', '/upload_files/2020-2-26/1582649943540.jpeg', NULL, NULL);
+INSERT INTO `navs` VALUES (2, 13, '0,1,0,0,1,1', '000,000,000', '0,0,0', '000,000,000', '000,000,000', '0,0,0', '0,0,0', 'http://192.168.0.104:7788/upload_files/2020-2-17/syh.jpg', NULL, NULL, NULL, NULL, NULL);
+INSERT INTO `navs` VALUES (3, 5, '1,1,1,1,1,1', '可爱至极,傻六六在此,标配顶配天仙配', '12345,54321,杀杀杀', '生活点滴,记录美好,拍出火气', '爱情～,一日三餐四季,你我足以', '旅行的意义,生命~,川流不息', '000,000,000', '/upload_files/2020-2-27/1582800976759.jpeg', '/upload_files/2020-2-28/1582865340493.jpg', '', '/upload_files/2020-2-27/1582800254625.jpg', '/upload_files/2020-2-27/1582799517765.jpg', '/upload_files/2020-2-27/1582800228597.jpg');
+INSERT INTO `navs` VALUES (9, 23, '1,1,1,1,1,1', '000,000,000', '000,000,000', '000,000,000', '000,000,000', '000,000,000', '000,000,000', '/upload_files/2020-2-28/1582867624793.gif', NULL, NULL, NULL, NULL, NULL);
+
+-- ----------------------------
+-- Table structure for resume
+-- ----------------------------
+DROP TABLE IF EXISTS `resume`;
+CREATE TABLE `resume`  (
+  `content` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL COMMENT '文章',
+  `user_id` int(11) NOT NULL COMMENT '用户ID',
+  PRIMARY KEY (`user_id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of resume
+-- ----------------------------
+INSERT INTO `resume` VALUES ('<p style=\"text-align: center;\">舒榆衡</p><p style=\"text-align: center;\">男：18岁<img src=\"http://192.168.0.104:8888/upload_files/2020-2-28/1582905233293.jpg\" style=\"max-width: 100%;\"></p><p style=\"text-align: center;\"><img src=\"http://192.168.0.104:8888/upload_files/2020-2-28/1582905311825.gif\" style=\"max-width:100%;\">&nbsp; 奔跑的张冰雪&nbsp; ，老子爱你哟<br></p>', 5);
+INSERT INTO `resume` VALUES ('465498641329687', 13);
 
 -- ----------------------------
 -- Table structure for users
@@ -100,12 +117,13 @@ CREATE TABLE `users`  (
   `username` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
   `head_src` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 14 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 24 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of users
 -- ----------------------------
-INSERT INTO `users` VALUES (5, '17768938296', 'syh654321', '撒开绿灯', '/upload_files/2020-2-17/syh.jpg');
-INSERT INTO `users` VALUES (13, '17768938295', '123456', '舒榆衡', NULL);
+INSERT INTO `users` VALUES (5, '17768938296', 'syh654321', '舒榆衡1', '/upload_files/2020-2-28/1582865362689.gif');
+INSERT INTO `users` VALUES (13, '17768938295', '123456', '舒榆衡', '/upload_files/2020-2-26/1582647646932.gif');
+INSERT INTO `users` VALUES (23, '17851800387', '123456', '张六六', '/upload_files/2020-2-28/1582867614626.gif');
 
 SET FOREIGN_KEY_CHECKS = 1;
